1deab113d4553b1bc0b8155f48a1ba08c520fbf8
rohit <rohitsoni181001@gmail.com>
2023-04-14 19:55:39 +0530

    first commmit

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,65 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const myApp());
 }
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-  // ignore: prefer_function_declarations_over_variables
 
-Expanded addkey( {Color color=Colors.green,int sound_id=1})
-  { return Expanded(child: OutlinedButton(
-    child:Container(
-      color: color,
-    ),
+class myApp extends StatefulWidget {
+  const myApp({Key? key}) : super(key: key);
 
-    onPressed: () async {
-      final player= AudioPlayer();
-  await  player.play(AssetSource('note$sound_id.wav'));
-    },
-  ),
-  );
+  @override
+  State<myApp> createState() => _myAppState();
+}
+
+class _myAppState extends State<myApp> {
+  void playSound(int songnum)
+  {
+    final player = AudioPlayer();
+    player.play(AssetSource('note$songnum.wav'));
+  }
+
+  Expanded buildkey({required Color coloname,required int songNumber})
+  {
+    return Expanded(
+      child: OutlinedButton(
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all(coloname),
+
+        ),
+        onPressed: () {
+          playSound(songNumber);
+        },
+        child: Text(''),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.black,
         body: SafeArea(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              addkey(color: Colors.green,sound_id: 1),
-              addkey(color: Colors.teal,sound_id: 2),
-              addkey(color: Colors.blue,sound_id: 3),
-              addkey(color: Colors.purple,sound_id:4),
-              addkey(color: Colors.red,sound_id: 5),
-              addkey(color: Colors.orange,sound_id: 6),
-              addkey(color: Colors.yellow,sound_id: 7),
+              buildkey(coloname: Colors.pinkAccent, songNumber: 1),
+              buildkey(coloname: Colors.white, songNumber: 2),
+              buildkey(coloname: Colors.blue, songNumber: 3),
+              buildkey(coloname: Colors.grey, songNumber: 4),
+              buildkey(coloname: Colors.yellow, songNumber: 5),
+              buildkey(coloname: Colors.pink, songNumber: 6),
+              buildkey(coloname: Colors.cyan, songNumber: 7),
+              OutlinedButton(
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.red),
+                ),
+                onPressed: () {
+                  final player = AudioPlayer();
+                  player.play(AssetSource('KabhiTumheShershaah.wav'));
+                },
+                child: Text('KabhiTumheShershaah.wav'),
+              ),
             ],
           ),
         ),
@@ -47,3 +67,5 @@ Expanded addkey( {Color color=Colors.green,int sound_id=1})
     );
   }
 }
+
+
